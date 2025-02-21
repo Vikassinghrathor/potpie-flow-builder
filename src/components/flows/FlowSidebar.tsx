39bfe5e14602d1a6ee1a2778b71e97b638e289b5
vikassinghrathor <rathorevikas2809@gmail.com>
2025-02-21 11:35:13 +0530

    complete the ui

--- src/components/flows/FlowSidebar.tsx
+++ src/components/flows/FlowSidebar.tsx
@@ -1,43 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import profile from "../../assets/profile.svg";
 import { Menu, History, Layers, FileCode } from "lucide-react";
 
-
 export const FlowSidebar = () => {
   return (
-    <div className="fixed left-0 top-0 h-screen w-16 flex flex-col bg-[#363636] py-4">
+    <aside className="fixed left-0 top-0 h-screen w-16 flex-col bg-[#363636] py-4 hidden sm:flex">
       {/* Logo Section */}
-      <div className="flex flex-col items-center gap-6">
-        <img src={logo} alt="logo" className="w-6 h-6" />
-        <button className="p-2 rounded hover:bg-gray-700 transition-colors">
+      <div className="flex flex-col items-center gap-6 mt-0.5">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-8 h-8" />
+        </Link>
+        <Link
+          to="#"
+          className="p-2 rounded hover:bg-gray-700 transition-colors"
+        >
           <Menu className="w-6 h-6 text-[#FFFFFF]" />
-        </button>
+        </Link>
       </div>
 
       {/* Navigation Icons */}
       <div className="flex flex-col items-center gap-4 mt-6">
-        <button className="p-2 rounded hover:bg-gray-700 transition-colors">
+        <Link
+          to="#"
+          className="p-2 rounded hover:bg-gray-700 transition-colors"
+        >
           <History className="w-6 h-6 text-[#FFFFFF]" />
-        </button>
-        <button className="p-2 rounded hover:bg-gray-700 transition-colors">
+        </Link>
+        <Link
+          to="#"
+          className="p-2 rounded hover:bg-gray-700 transition-colors"
+        >
           <Layers className="w-6 h-6 text-[#FFFFFF]" />
-        </button>
-        <button className="p-2 rounded hover:bg-gray-700 transition-colors">
+        </Link>
+        <Link
+          to="#"
+          className="p-2 rounded hover:bg-gray-700 transition-colors"
+        >
           <img
             loading="lazy"
             src="https://cdn.builder.io/api/v1/image/assets/ddf789af42b44eaea963851b07583965/fd429ffca8e797f5e80a4540736a44b5d016dcdef6fc3ba570ab72f8f50dacf8?placeholderIfAbsent=true"
             className="w-6 h-6 text-[#FFFFFF]"
             alt="github icon"
           />
-        </button>
+        </Link>
       </div>
 
       {/* Profile Section */}
       <div className="mt-auto flex justify-center">
-        <img src={profile} alt="Profile" className="w-8 h-8 rounded-full" />
+        <Link to="#">
+          <img src={profile} alt="Profile" className="w-8 h-8 rounded-full" />
+        </Link>
       </div>
-    </div>
+    </aside>
   );
 };
 
